a4fb798952ea0e29acefd12db6501f569d241f76
ross96D <abelaross96@gmail.com>
2025-06-26 23:07:48 -0400

    add predefined declarations

--- lib/src/config_base.dart
+++ lib/src/config_base.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:config/src/evaluator/evaluator.dart';
@@ -6,7 +5,10 @@ import 'package:config/src/lexer/lexer.dart';
 import 'package:config/src/parser/parser.dart';
 
 class ConfigurationParser {
-  static (MapValue?, List<ParseError>?) parseFromFile(File file) {
+  static (MapValue?, List<ParseError>?) parseFromFile(
+    File file, [
+    Map<String, String> predefinedDeclarations = const {},
+  ]) {
     String content = file.readAsStringSync();
     final lexer = Lexer(content);
     final parser = Parser(lexer);
@@ -14,6 +16,10 @@ class ConfigurationParser {
     if (parser.errors.isNotEmpty) {
       return (null, parser.errors);
     }
-    return (Evaluator(program).eval(), null);
+    final evaluator = Evaluator(program);
+    evaluator.declarations.addAll(
+      predefinedDeclarations.map((k, v) => MapEntry(k, StringValue(v))),
+    );
+    return (evaluator.eval(), null);
   }
 }
