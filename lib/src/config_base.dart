e82b6639257db4585245a458b4adf5b8369a589d
ross96D <abelaross96@gmail.com>
2025-06-26 22:28:00 -0400

    add parseFromFile

--- lib/src/config_base.dart
+++ lib/src/config_base.dart
@@ -1,6 +1,19 @@
-// TODO: Put public facing types in this file.
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'dart:io';
+
+import 'package:config/src/evaluator/evaluator.dart';
+import 'package:config/src/lexer/lexer.dart';
+import 'package:config/src/parser/parser.dart';
+
+class ConfigurationParser {
+  static (MapValue?, List<ParseError>?) parseFromFile(File file) {
+    String content = file.readAsStringSync();
+    final lexer = Lexer(content);
+    final parser = Parser(lexer);
+    final program = parser.parseProgram();
+    if (parser.errors.isNotEmpty) {
+      return (null, parser.errors);
+    }
+    return (Evaluator(program).eval(), null);
+  }
 }
