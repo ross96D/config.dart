d0cc34d3be54c731e4c6862ecddbd35f634be19e
ross96D <abelaross96@gmail.com>
2025-07-19 15:20:48 -0400

    merge test command to improve execution speed

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-echo -e "\033[0;34mdart test lib/src/lexer/lexer_test.dart \033[0m"
-dart test lib/src/lexer/lexer_test.dart
+# echo -e "\033[0;34mdart test lib/src/lexer/lexer_test.dart \033[0m"
+# dart test lib/src/lexer/lexer_test.dart
 
-echo -e "\n\033[0;34mdart test lib/src/parser/parser_test.dart \033[0m"
-dart test lib/src/parser/parser_test.dart
+# echo -e "\n\033[0;34mdart test lib/src/parser/parser_test.dart \033[0m"
+# dart test lib/src/parser/parser_test.dart
 
-echo -e "\n\033[0;34mdart test lib/src/evaluator/evaluator_test.dart \033[0m"
-dart test lib/src/evaluator/evaluator_test.dart
+# echo -e "\n\033[0;34mdart test lib/src/evaluator/evaluator_test.dart \033[0m"
+# dart test lib/src/evaluator/evaluator_test.dart
+
+echo -e "\n\033[0;34mdart test lib/src/lexer/lexer_test.dart lib/src/parser/parser_test.dart lib/src/evaluator/evaluator_test.dart \033[0m"
+dart test lib/src/lexer/lexer_test.dart lib/src/parser/parser_test.dart lib/src/evaluator/evaluator_test.dart
